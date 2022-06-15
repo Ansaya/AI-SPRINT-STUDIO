71713cb6da26366f6a27e0e1cd82054989fd3015
Francesco Lattari;Francesco Lattari <francesco.lattari@polimi.it>
2022-06-15 21:06:45 +0200

    Add mask detection AI-SPRINT application + minor

--- examples/ai-sprint_simple_app/src/component_1/main.py
+++ examples/ai-sprint_simple_app/src/component_1/main.py
@@ -1,8 +1,8 @@
 import os
 
-from aisprint.annotations import exec_time, component_name, annotation
+from aisprint.annotations import annotation
 
-@annotation({'component_name': {'name': 'C1'}, 'exec_time': {'local_time_thr': 500}})
+@annotation({'component_name': {'name': 'C1'}, 'exec_time': {'local_time_thr': 5}})
 def main(args):
     print("Hello! I'm an example component C1!")
 
